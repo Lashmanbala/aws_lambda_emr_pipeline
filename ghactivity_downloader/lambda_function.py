b906d415fcbc53ebcb663244143ded12eb98071e
lashmanbala <lashmanbala@gmail.com>
2024-07-05 18:01:20 +0530

    updated the lambda function to download the file

--- ghactivity_downloader/lambda_function.py
+++ ghactivity_downloader/lambda_function.py
@@ -1,8 +1,9 @@
 import json
+from download import download_file
 
 def lambda_handler(event, context):
-    # TODO implement
+    download_res = download_file('2015-01-01-15.json.gz')
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from local_Lambda!')
+        'statusCode': download_res.status_code,
+        'body': json.dumps('downloaded the file')
     }
