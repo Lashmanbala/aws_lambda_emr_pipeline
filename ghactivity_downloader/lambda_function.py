0fe3bf6e0afa76304189cce5792303b477289fca
lashmanbala <lashmanbala@gmail.com>
2024-07-06 13:01:59 +0530

    updated lambda function with upload functionality for local deployment

--- ghactivity_downloader/lambda_function.py
+++ ghactivity_downloader/lambda_function.py
@@ -1,10 +1,16 @@
 import json
 from download import download_file
+from upload import upload_s3
 
 def lambda_handler(event, context):
-    download_res = download_file('2015-01-01-15.json.gz')
+    file = '2015-01-01-16.json.gz'
+
+    download_res = download_file(file)
+    upload_res = upload_s3('gh-raw-bucket',file,download_res.content)
+
     return {
-        'statusCode': download_res.status_code,
+        'statusCode': upload_res,
         'body': json.dumps('downloaded the file')
     }
+
 print(lambda_handler(None, None))
