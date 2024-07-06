508969a696f38c603fba884a7ad8a8f2752c4fde
lashmanbala <lashmanbala@gmail.com>
2024-07-06 18:31:23 +0530

    doenloading the fle in incremental fashion if it available in the site

--- ghactivity_downloader/util.py
+++ ghactivity_downloader/util.py
@@ -1,8 +1,12 @@
 from datetime import datetime, timedelta
+import requests
 
 next_file ='2024-07-06-0.json.gz'
 
-for i in range(24):
+while True:
+    res = requests.get(f'https://data.gharchive.org/{next_file}')
+    if res.status_code != 200:
+        break
+    print(f'The status code for {next_file} is {res.status_code}')
     date_part = next_file.split('.')[0]
     next_file = f"{datetime.strftime(datetime.strptime(date_part, '%Y-%M-%d-%H')+timedelta(hours=1), '%Y-%M-%d-%-H')}.json.gz"
-    print(next_file)
