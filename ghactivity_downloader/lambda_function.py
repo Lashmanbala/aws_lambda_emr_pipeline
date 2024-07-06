6a3642c2ab91faedf00318362599ddeb509ada8a
lashmanbala <lashmanbala@gmail.com>
2024-07-06 14:10:36 +0530

    added environment variable for deployement in lambda

--- ghactivity_downloader/lambda_function.py
+++ ghactivity_downloader/lambda_function.py
@@ -1,12 +1,14 @@
 import json
+import os
 from download import download_file
 from upload import upload_s3
 
 def lambda_handler(event, context):
-    file = '2015-01-01-16.json.gz'
+    file = '2015-01-01-17.json.gz'
 
+    bucket = os.environ.get('BUCKET_NAME')
     download_res = download_file(file)
-    upload_res = upload_s3('gh-raw-bucket',file,download_res.content)
+    upload_res = upload_s3(bucket,file,download_res.content)
 
     return {
         'statusCode': upload_res,
