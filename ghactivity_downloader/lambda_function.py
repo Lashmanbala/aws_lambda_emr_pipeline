8e72d09776eca3950e588b9bbeeb6db0673c879c
lashmanbala <lashmanbala@gmail.com>
2024-07-06 16:12:35 +0530

    added file prefix

--- ghactivity_downloader/lambda_function.py
+++ ghactivity_downloader/lambda_function.py
@@ -4,11 +4,13 @@ from download import download_file
 from upload import upload_s3
 
 def lambda_handler(event, context):
-    file = '2015-01-01-17.json.gz'
+    file = '2015-01-01-15.json.gz'
 
     bucket = os.environ.get('BUCKET_NAME')
+    file_prefix = os.environ.get('FILE_PREFIX')
+
     download_res = download_file(file)
-    upload_res = upload_s3(bucket,file,download_res.content)
+    upload_res = upload_s3(bucket,f'{file_prefix}/{file}',download_res.content)
 
     return {
         'statusCode': upload_res,
