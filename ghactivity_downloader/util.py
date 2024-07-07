eae68f591c6c8f6881580fd26ddc270d9c7039f1
lashmanbala <lashmanbala@gmail.com>
2024-07-07 06:53:25 +0530

    creating bookmark of previous file and maintaining it in s3 and using it to download the next file in incremental fashion

--- ghactivity_downloader/util.py
+++ ghactivity_downloader/util.py
@@ -1,12 +1,33 @@
 from datetime import datetime, timedelta
-import requests
+import requests, boto3, os
+from botocore.errorfactory import ClientError
 
-next_file ='2024-07-06-0.json.gz'
+s3_client = boto3.client('s3')
+
+baseline_file ='2024-07-06-0.json.gz'
 
 while True:
+    try:
+        bookmark_file = s3_client.get_object(Bucket='gh-raw-bucket',
+                                    Key='sandbox/bookmark')
+        prev_file = bookmark_file['Body'].read().decode('utf-8')
+    except ClientError as e:
+        if e.response['Error']['Code']=='NoSuchKey':
+            prev_file = baseline_file
+        else:
+            raise
+
+    date_part = prev_file.split('.')[0]
+    next_file = f"{datetime.strftime(datetime.strptime(date_part, '%Y-%M-%d-%H')+timedelta(hours=1), '%Y-%M-%d-%-H')}.json.gz"
+
     res = requests.get(f'https://data.gharchive.org/{next_file}')
+
     if res.status_code != 200:
         break
     print(f'The status code for {next_file} is {res.status_code}')
-    date_part = next_file.split('.')[0]
-    next_file = f"{datetime.strftime(datetime.strptime(date_part, '%Y-%M-%d-%H')+timedelta(hours=1), '%Y-%M-%d-%-H')}.json.gz"
+
+    bookmark_content = next_file
+    s3_client.put_object(Bucket='gh-raw-bucket',
+                                    Key='sandbox/bookmark',
+                                    Body=bookmark_content.encode('utf-8'))
+
