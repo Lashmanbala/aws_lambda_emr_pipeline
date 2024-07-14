90f8d5e5ec8cf2ca6c04ce5608f2d281c515da67
lashmanbala <lashmanbala@gmail.com>
2024-07-14 12:38:32 +0530

    Write functionality

--- pyspark/app.py
+++ pyspark/app.py
@@ -2,7 +2,7 @@ import os
 from util import get_spark_session
 from read import from_files
 from process import transform
-
+from write import to_files
 
 def main():
     env = os.environ.get('ENVIRON')
@@ -10,14 +10,16 @@ def main():
     src_dir = os.environ.get('SRC_DIR')
     src_file_pattern = f"{os.environ.get('SRC_FILE_PATTERN')}-*" # 24 files per day
     src_file_format = os.environ.get('SRC_FILE_FORMAT')
-
+    tgt_dir = os.environ.get('TGT_DIR')
+    tgt_file_format = os.environ.get('TGT_FILE_FORMAT')
+    
     spark = get_spark_session(env, 'Github')
 
     df = from_files(spark,src_file_format,src_dir,src_file_pattern)
 
     df_transformed = transform(df)
-    df_transformed.printSchema()
-    df_transformed.select('repo.*','created_at','year','month','day').show()
+    
+    to_files(df_transformed,tgt_dir,tgt_file_format)
 
 if __name__ == '__main__':
     main()
