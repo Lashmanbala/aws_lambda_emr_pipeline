27d876b5fa36f946fe6812bc377656defb872f49
lashmanbala <lashmanbala@gmail.com>
2024-07-20 14:24:06 +0530

    Upload functionality

--- aws_resources/app.py
+++ aws_resources/app.py
@@ -1,4 +1,13 @@
-from util import create_bucket
+from util import create_bucket, upload_s3
 
-bkt = create_bucket('bala-test-3')
-print(bkt)
+bkt_res = create_bucket(bkt_name='github-bkt')
+
+if bkt_res['ResponseMetadata']['HTTPStatusCode'] == 200:
+        print('Bucket created successfully')
+
+        upload_res = upload_s3(bucket='github-bkt',
+                folder='zipfiles',
+                file_name='zip_file_for_lambda.zip',
+                body='/home/bala/code/projects/github_activity_project/ghactivity_downloader/ghactivity_downloader_for_lambda.zip')
+                
+        print('zipfile uploded successfully')
--- aws_resources/util.py
+++ aws_resources/util.py
@@ -6,3 +6,11 @@ def create_bucket(bkt_name):
         
     return res
 
+def upload_s3(bucket,folder,file_name,body):
+    s3_client = boto3.client('s3')
+    res = s3_client.put_object(
+            Bucket=bucket,
+            Key=f'{folder}/{file_name}',
+            Body=body
+            )
+    return res
