37ee57bc8bbeb36d7a696ba27c3afcf7db5a9513
lashmanbala <lashmanbala@gmail.com>
2024-07-21 18:29:58 +0530

    Adding target to the event rule and adding required permission to lambda function

--- aws_resources/util.py
+++ aws_resources/util.py
@@ -94,11 +94,11 @@ def invoke_lambda_funtion(func_name):
     except Exception as e:
         print(e)
 
-def create_event_bridge_rule():
+def create_event_bridge_rule(rule_name):
     event_bridge_client = boto3.client('events')
     try:
         event_rule_response = event_bridge_client.put_rule(
-                Name='HourlyGhactivityDownloadRule',
+                Name=rule_name,
                 ScheduleExpression='rate(60 minutes)',
                 State='ENABLED',
                 Description='Trigger ghactivity-download-function hourly'
@@ -107,3 +107,28 @@ def create_event_bridge_rule():
     except Exception as e:
         print(e)
 
+def add_target_to_rule(rule_name, lambda_arn, rule_arn):
+    eventbridge_client = boto3.client('events')
+    lambda_client = boto3.client('lambda')
+    try:
+        put_targets_response = eventbridge_client.put_targets(
+        Rule=rule_name,
+        Targets=[
+            {
+                'Id': '1',
+                'Arn': lambda_arn,
+            }
+        ]
+        )
+        # Add permission for EventBridge to invoke the Lambda function
+        lambda_client.add_permission(
+            FunctionName=lambda_arn,
+            StatementId='EventBridgeInvokeLambda',
+            Action='lambda:InvokeFunction',
+            Principal='events.amazonaws.com',
+            SourceArn=rule_arn
+        )
+        
+        return put_targets_response
+    except Exception as e:
+        print(e)
