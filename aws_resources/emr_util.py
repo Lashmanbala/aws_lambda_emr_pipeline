90772100f19398a4a400f100503e5dd8d695acc3
lashmanbala <lashmanbala@gmail.com>
2024-07-24 08:35:21 +0530

    Modified function results

--- aws_resources/emr_util.py
+++ aws_resources/emr_util.py
@@ -21,12 +21,13 @@ def create_emr_ec2_instance_profile():
     # Create the IAM role
     try:
         role_response = iam_client.create_role(
-            RoleName='EMR_EC2_Role',
-            AssumeRolePolicyDocument=json.dumps(role_policy_document),
-            Description='Role for EMR EC2 instances'
-        )
+                                                RoleName='EMR_EC2_Role',
+                                                AssumeRolePolicyDocument=json.dumps(role_policy_document),
+                                                Description='Role for EMR EC2 instances'
+                                            )
         role_arn = role_response['Role']['Arn']
         print(f'Role "EMR_EC2_Role" created successfully with ARN: {role_arn}')
+
     except iam_client.exceptions.EntityAlreadyExistsException:
         print('Role "EMR_EC2_Role" already exists.')
         role_arn = iam_client.get_role(RoleName='EMR_EC2_Role')['Role']['Arn']
@@ -34,6 +35,7 @@ def create_emr_ec2_instance_profile():
     # Attach the AmazonElasticMapReduceforEC2Role and the s3 fullaccess policy
     emr_policy_arn = 'arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role'
     s3_policy_arn = 'arn:aws:iam::aws:policy/AmazonS3FullAccess'
+
     arn_list = [emr_policy_arn, s3_policy_arn]
 
     for arn in arn_list:    
@@ -48,15 +50,18 @@ def create_emr_ec2_instance_profile():
     try:
         iam_client.create_instance_profile(InstanceProfileName='EMR_EC2_InstanceProfile')
         print('Instance profile "EMR_EC2_InstanceProfile" created successfully.')
+
     except iam_client.exceptions.EntityAlreadyExistsException:
         print('Instance profile "EMR_EC2_InstanceProfile" already exists.')
 
     # Add role to instance profile
-    iam_client.add_role_to_instance_profile(
-        InstanceProfileName='EMR_EC2_InstanceProfile',
-        RoleName='EMR_EC2_Role'
-    )
+    response = iam_client.add_role_to_instance_profile(
+                                                    InstanceProfileName='EMR_EC2_InstanceProfile',
+                                                    RoleName='EMR_EC2_Role'
+                                                    )
     print('Role "EMR_EC2_Role" added to instance profile "EMR_EC2_InstanceProfile".')
+    return response["InstanceProfile"]["InstanceProfileName"]
+    
 
 def create_emr_service_role():
     iam_client = boto3.client('iam')
@@ -78,25 +83,25 @@ def create_emr_service_role():
     # Create the IAM role
     try:
         role_response = iam_client.create_role(
-            RoleName='EMR_Service_Role',
-            AssumeRolePolicyDocument=json.dumps(role_policy_document),
-            Description='Role for EMR service'
-        )
-        role_arn = role_response['Role']['Arn']
-        print(f'Role "EMR_Service_Role" created successfully with ARN: {role_arn}')
+                                                RoleName='EMR_Service_Role',
+                                                AssumeRolePolicyDocument=json.dumps(role_policy_document),
+                                                Description='Role for EMR service'
+                                            )
+        role_name = role_response['Role']['RoleName']
+        print(f'Role "EMR_Service_Role" created successfully with ARN: {role_name}')
+
     except iam_client.exceptions.EntityAlreadyExistsException:
         print('Role "EMR_Service_Role" already exists.')
-        role_arn = iam_client.get_role(RoleName='EMR_Service_Role')['Role']['Arn']
+        role_name = iam_client.get_role(RoleName='EMR_Service_Role')['Role']['RoleName']
 
     # Attach the AmazonElasticMapReduceRole policy
     emr_policy_arn = 'arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole'
     iam_client.attach_role_policy(
-        RoleName='EMR_Service_Role',
-        PolicyArn=emr_policy_arn
-    )
-    print(f'Policy {emr_policy_arn} attached to role "EMR_Service_Role".')
-
-    return role_arn
+                                RoleName='EMR_Service_Role',
+                                PolicyArn=emr_policy_arn
+                            )
+    print(f'Policy {emr_policy_arn} attached to role "{role_name}".')
+    return role_name
 
 def create_emr_cluster(bucket_name, instance_type, core_instance_count):
         
@@ -132,13 +137,16 @@ def create_emr_cluster(bucket_name, instance_type, core_instance_count):
             {'Name': 'Spark'}
         ],
         'JobFlowRole': 'EMR_EC2_InstanceProfile',  # Ensure this role exists or create it 
-        'ServiceRole': 'EMR_Service_Role'  # Ensure this role exists or create it 
+        'ServiceRole': 'EMR_Service_Role',  # Ensure this role exists or create it
+        'AutoTerminationPolicy': {
+            'IdleTimeout': 300
+        }
     }
 
     # Create the cluster with the step
     emr_response = emr_client.run_job_flow(**cluster_config)
 
-    # Print the cluster ID
+    print('Successfully cluter created')
     return emr_response['JobFlowId']
 
 
@@ -169,14 +177,10 @@ def add_spark_step(cluster_id, env_vars_dict, zip_file_path, app_file_path):
     try:
         # Add the step to the cluster
         step_response = emr_client.add_job_flow_steps(
-            JobFlowId=cluster_id,
-            Steps=[step_config]
-        )
+                                                    JobFlowId=cluster_id,
+                                                    Steps=[step_config]
+                                                )
 
-        # Print the response for debugging purposes (optional)
-        # print(step_response)
-
-        # Return the step ID
         return step_response['StepIds'][0]
     except Exception as e:
         print(e)
