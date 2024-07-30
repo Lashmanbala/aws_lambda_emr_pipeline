3956dfb8fc35ec28b1b6cd2382701b511bd38eac
lashmanbala <lashmanbala@gmail.com>
2024-07-30 14:42:13 +0530

    Tested bookmars in local

--- pyspark/app.py
+++ pyspark/app.py
@@ -3,23 +3,55 @@ from util import get_spark_session
 from read import from_files
 from process import transform
 from write import to_files
+from bookmark import get_prev_day, get_next_day, upload_bookmark
+from datetime import datetime
 
+# 2024-07-21-0.json.gz
 def main():
-    env = os.environ.get('ENVIRON')
+    # env = os.environ.get('ENVIRON')
+    # src_dir = os.environ.get('SRC_DIR')
+    # src_file_pattern = f"{os.environ.get('SRC_FILE_PATTERN')}-*" # 24 files per day
+    # src_file_format = os.environ.get('SRC_FILE_FORMAT')
+    # tgt_dir = os.environ.get('TGT_DIR')
+    # tgt_file_format = os.environ.get('TGT_FILE_FORMAT')
+    # bucket_name = os.environ.get('BUCKET_NAME')
+    # file_prefix = os.environ.get('FILE_PREFIX')
+    # bookmark_file = os.environ.get('BOOKMARK_FILE')
+    # baseline_file = os.environ.get('BASELINE_FILE')
     
-    src_dir = os.environ.get('SRC_DIR')
-    src_file_pattern = f"{os.environ.get('SRC_FILE_PATTERN')}-*" # 24 files per day
-    src_file_format = os.environ.get('SRC_FILE_FORMAT')
-    tgt_dir = os.environ.get('TGT_DIR')
-    tgt_file_format = os.environ.get('TGT_FILE_FORMAT')
-    
+    env = 'DEV'
+    src_dir = '/pyspark/sample_data/'
+    src_file_pattern = '2015-01-01' # 24 files per day
+    src_file_format = 'json'
+    tgt_dir = '/pyspark/target/'
+    tgt_file_format = 'parquet'
+    local_directory = '/pyspark/'
+    bookmark_file = 'bookmark.txt'
+    baseline_file = '2024-07-27-0.json.gz'
+
+
     spark = get_spark_session(env, 'Github')
 
-    df = from_files(spark,src_file_format,src_dir,src_file_pattern)
+    while True:
+        # prev_day = get_prev_day(bucket_name,file_prefix,bookmark_file,baseline_file)
+        # nxt_day = get_next_day(prev_day)
+        prev_day = get_prev_day(local_directory, bookmark_file, baseline_file)
+        nxt_day = get_next_day(prev_day)
 
-    df_transformed = transform(df)
-    
-    to_files(df_transformed,tgt_dir,tgt_file_format)
+        src_file_pattern = f'{nxt_day}-*'
+
+        df = from_files(spark,src_file_format,src_dir,src_file_pattern)
+
+        df_transformed = transform(df)
+        
+        to_files(df_transformed,tgt_dir,tgt_file_format)
+
+        if datetime.strptime(nxt_day, '%Y-%m-%d').date() == datetime.today().date():
+            print('its today')
+            break
+
+        # upload_bookmark(bucket_name,file_prefix,bookmark_file,nxt_day)
+        upload_bookmark(local_directory, bookmark_file, nxt_day)
 
 if __name__ == '__main__':
     main()
--- pyspark/bookmark.py
+++ pyspark/bookmark.py
@@ -1,45 +1,60 @@
-import boto3
-from botocore.errorfactory import ClientError
+# import boto3
+# from botocore.errorfactory import ClientError
 from datetime import datetime, timedelta
 
-bucket_name = 'github-bkt1'
-file_prefix = 'zipfiles1'
+# bucket_name = 'github-bkt1'
+# file_prefix = 'zipfiles1'
 bookmark_file = 'bookmark'
-baseline_file = '2024-07-26-0.json.gz'
-def get_prev_day(bucket_name,file_prefix,bookmark_file,baseline_file):
-    s3_client = boto3.client('s3')
+baseline_file = '2015-01-01-0.json.gz'
+# def get_prev_day(bucket_name,file_prefix,bookmark_file,baseline_file):
+#     s3_client = boto3.client('s3')
+#     try:
+#         bookmark_file = s3_client.get_object(Bucket=bucket_name,
+#                                     Key=f'{file_prefix}/{bookmark_file}')
+#         prev_day = bookmark_file['Body'].read().decode('utf-8')
+#     except ClientError as e:
+#         if e.response['Error']['Code']=='NoSuchKey':
+#             prev_day = baseline_file.split('.')[0][:-2]
+#         else:
+#             raise
+#     return prev_day
+
+# def get_next_day(prev_day):
+#     next_day = f"{datetime.strftime(datetime.strptime(prev_day, '%Y-%M-%d')+timedelta(days=1), '%Y-%M-%d')}"
+#     return next_day
+
+# def upload_bookmark(bucket_name,file_prefix,bookmark_file,bookmark_content):
+#     s3_client = boto3.client('s3')
+#     s3_client.put_object(Bucket=bucket_name,
+#                         Key=f'{file_prefix}/{bookmark_file}',
+#                         Body=bookmark_content.encode('utf-8'))
+
+import os
+from datetime import datetime, timedelta
+
+def get_prev_day(local_directory, bookmark_file, baseline_file):
+    bookmark_path = os.path.join(local_directory, bookmark_file)
     try:
-        bookmark_file = s3_client.get_object(Bucket=bucket_name,
-                                    Key=f'{file_prefix}/{bookmark_file}')
-        prev_day = bookmark_file['Body'].read().decode('utf-8')
-    except ClientError as e:
-        if e.response['Error']['Code']=='NoSuchKey':
-            prev_day = baseline_file.split('.')[0][:-2]
-        else:
-            raise
+        with open(bookmark_path, 'r') as f:
+            prev_day = f.read().strip()
+    except FileNotFoundError:
+        prev_day = baseline_file.split('.')[0][:-2]
     return prev_day
 
 def get_next_day(prev_day):
-    next_day = f"{datetime.strftime(datetime.strptime(prev_day, '%Y-%M-%d')+timedelta(days=1), '%Y-%M-%d')}"
+    next_day = f"{datetime.strftime(datetime.strptime(prev_day, '%Y-%m-%d') + timedelta(days=1), '%Y-%m-%d')}"
     return next_day
 
-def upload_bookmark(bucket_name,file_prefix,bookmark_file,bookmark_content):
-    s3_client = boto3.client('s3')
-    s3_client.put_object(Bucket=bucket_name,
-                        Key=f'{file_prefix}/{bookmark_file}',
-                        Body=bookmark_content.encode('utf-8'))
+def upload_bookmark(local_directory, bookmark_file, bookmark_content):
+    bookmark_path = os.path.join(local_directory, bookmark_file)
+    with open(bookmark_path, 'w') as f:
+        f.write(bookmark_content)
 
-while True:
-    prev_day = get_prev_day(bucket_name,file_prefix,bookmark_file,baseline_file)
-    print(prev_day)
-    print(type(prev_day))
-    nxt_day = get_next_day(prev_day)
-    print(nxt_day)
-    print(type(nxt_day))
-    nd = datetime.strptime(nxt_day, '%Y-%m-%d').date()
-    print(nd)
-    print(type(nd))
-    if datetime.strptime(nxt_day, '%Y-%m-%d').date() == datetime.today().date():
-        print('its today')
-        break
-    upload_bookmark(bucket_name,file_prefix,bookmark_file,nxt_day)
+# Example usage:
+# local_directory = '/home/bala/code/projects/github_activity_project/pyspark/'
+# bookmark_file = 'bookmark.txt'
+# baseline_file = '2015-01-01-0.json.gz'
+
+# prev_day = get_prev_day(local_directory, bookmark_file, baseline_file)
+# next_day = get_next_day(prev_day)
+# upload_bookmark(local_directory, bookmark_file, next_day)
