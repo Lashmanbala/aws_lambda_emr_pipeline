50cfc0130009a2257ea162681114a7043793a248
lashmanbala <lashmanbala@gmail.com>
2026-01-29 20:10:13 +0530

    testing

--- aws_resources/app.py
+++ aws_resources/app.py
@@ -1,10 +1,11 @@
 from s3_util import create_bucket, upload_s3
 from lambda_util import create_iam_role, create_lambda_function, invoke_lambda_funtion
 from event_bridge_util import create_event_bridge_rule, add_target_to_rule
+import dotenv
 
 def create_and_upload_s3():
     print('Creating bucket')
-    bucket='github-bkt3'
+    bucket='github-activity-bucket-123'
     bucket_res = create_bucket(bucket)
 
     if bucket_res['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -44,7 +45,7 @@ def create_downloder_lambda():
     lambda_s3_role_arn = create_role_response['Role']['Arn']
     print(f'IAM role created with ARN: {lambda_s3_role_arn}')
 
-    bucket='github-bkt3'
+    bucket='github-activity-bucket-123'
     folder='zipfiles'
     ghactivity_lambda_zipfile = '/home/bala/code/projects/github_activity_project/ghactivity_downloader/ghactivity_downloader_for_lambda.zip'
     file_name = ghactivity_lambda_zipfile.split('/')[-1]
@@ -52,7 +53,7 @@ def create_downloder_lambda():
     env_variables_dict = {'BUCKET_NAME' : bucket,
                         'FILE_PREFIX' : 'landing',
                         'BOOKMARK_FILE' : 'bookmark',
-                        'BASELINE_FILE' : '2024-08-01-20.json.gz'
+                        'BASELINE_FILE' : '2026-01-27-0.json.gz'
                         }
     func_name='ghactivity-download-function'
     handler = 'lambda_function.lambda_handler'
@@ -90,20 +91,19 @@ def create_emr_lambda():
     create_role_response = create_iam_role(role_name, policy_arn_list)
 
     lambda_s3_iam_emr_role_arn = create_role_response['Role']['Arn']
-    print(f'IAM role created with ARN: {lambda_s3_iam_emr_role_arn}')
 
-    bucket = 'github-bkt3'
+    bucket = 'github-activity-bucket-123'
     folder = 'zipfiles'
     emr_lambda_zipfile = '/home/bala/code/projects/github_activity_project/aws_resources/lambda_for_emr.zip'
     file_name = emr_lambda_zipfile.split('/')[-1]
     env_variables_dict = {
-        'BUCKET_NAME': 'github-bkt3',
-        'INSTANCE_TYPE': 'm4.xlarge',
+        'BUCKET_NAME': 'github-activity-bucket-123',
+        'INSTANCE_TYPE': 'm4.xlarge', # 4vcpu, 16 gb memory
         'CORE_INSTANCE_COUNT': '1',
-        'BOOTSTRAP_FILE_PATH': 's3://github-bkt3/zipfiles/install_boto3.sh',
-        'SPARK_ENV_DICT': '{"ENVIRON":"PROD", "SRC_DIR":"s3://github-bkt3/landing/", "SRC_FILE_FORMAT":"json", "TGT_DIR":"s3://github-bkt3/raw/", "TGT_FILE_FORMAT":"parquet","BUCKET_NAME":"github-bkt3","FILE_PREFIX":"raw","BOOKMARK_FILE":"bookmark","BASELINE_FILE":"2024-08-01-20.json.gz"}',
-        'ZIP_FILE_PATH': 's3://github-bkt3/zipfiles/github_spark_app.zip',
-        'APP_FILE_PATH': 's3://github-bkt3/zipfiles/app.py'
+        'BOOTSTRAP_FILE_PATH': 's3://github-activity-123/zipfiles/install_boto3.sh',
+        'SPARK_ENV_DICT': '{"ENVIRON":"PROD", "SRC_DIR":"s3://github-activity-bucket-123/landing/", "SRC_FILE_FORMAT":"json", "TGT_DIR":"s3://github-activity-bucket-123/raw/", "TGT_FILE_FORMAT":"parquet","BUCKET_NAME":"github-activity-bucket-123","FILE_PREFIX":"raw","BOOKMARK_FILE":"bookmark","BASELINE_FILE":"2026-01-27-0.json.gz"}',
+        'ZIP_FILE_PATH': 's3://github-activity-bucket-123/zipfiles/github_spark_app.zip',
+        'APP_FILE_PATH': 's3://github-activity-bucket-123/zipfiles/app.py'
     }
 
     func_name = 'lambda_function_for_emr'
@@ -129,7 +129,6 @@ def schedule_emr_lambda(lambda_arn):
 
 def deploy():
     create_and_upload_s3()
-
     downloader_lambda_arn = create_downloder_lambda()
     shedule_downloder_lambda(downloader_lambda_arn)
 
@@ -137,4 +136,5 @@ def deploy():
     schedule_emr_lambda(emr_lambda_arn)
 
 if __name__ == '__main__':
+    dotenv.load_dotenv()
     deploy()
