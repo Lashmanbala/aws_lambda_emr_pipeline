16696784ef0ad754f86a30eec178541bf414180d
lashmanbala <lashmanbala@gmail.com>
2026-02-09 14:31:25 +0530

    Reading

--- pyspark/read.py
+++ pyspark/read.py
@@ -1,5 +1,6 @@
-def from_files(spark, file_format, data_dir, file_pattern):
+def read_landing(spark, file_format, data_dir, file_pattern):
     df = spark.read. \
             format(file_format). \
             load(f'{data_dir}/{file_pattern}')
     return df
+# For json date spark automatically identifies schema
