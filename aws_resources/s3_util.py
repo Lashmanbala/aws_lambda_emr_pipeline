cf2c5ee2fcc1049130610f594e12d29815406fe6
lashmanbala <lashmanbala@gmail.com>
2026-02-12 19:11:45 +0530

    Updated

--- aws_resources/s3_util.py
+++ aws_resources/s3_util.py
@@ -2,12 +2,16 @@ import boto3
 
 def create_bucket(bucket_name):
     s3_client = boto3.client('s3')
+
     res = s3_client.create_bucket(Bucket=bucket_name)
         
     return res
 
+
 def upload_s3(bucket_name,folder,file_name,body):
+
     s3_client = boto3.client('s3')
+    
     res = s3_client.put_object(
                             Bucket=bucket_name,
                             Key=f'{folder}/{file_name}',
