284de3d00ea7e92b6ab6105e6827b03d0cf832fc
lashmanbala <lashmanbala@gmail.com>
2026-02-12 19:10:26 +0530

    Printing messages in the lambda console

--- aws_resources/lambda_function_for_emr.py
+++ aws_resources/lambda_function_for_emr.py
@@ -5,9 +5,11 @@ from emr_util import create_emr_ec2_instance_profile, create_emr_service_role, c
 
 def lambda_handler(event, context):
     emr_ec2_instance_profile = create_emr_ec2_instance_profile()
+    
     print(f' instance profile {emr_ec2_instance_profile} is created')
 
     emr_service_role = create_emr_service_role()
+
     print(f'IAM role {emr_service_role} is created')
 
     bucket_name= os.environ.get('BUCKET_NAME')
@@ -17,6 +19,7 @@ def lambda_handler(event, context):
     
     emr_response = create_emr_cluster(bucket_name, instance_type, core_instance_count, bootstrap_file_path)
     emr_cluster_id = emr_response['JobFlowId']
+
     print(f'cluster is created with the id {emr_cluster_id}')
 
     env_vars_dict= json.loads(os.environ.get('SPARK_ENV_DICT'))
@@ -24,4 +27,5 @@ def lambda_handler(event, context):
     app_file_path = os.environ.get('APP_FILE_PATH')
 
     step_response = add_spark_step(emr_cluster_id,env_vars_dict, zip_file_path, app_file_path)
+
     print(f"Successfully step is added to cluster. step id is {step_response['StepIds'][0]}")
