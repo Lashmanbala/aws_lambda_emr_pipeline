dd51345d01b1e06d63aae57625108c9bc18df8fc
lashmanbala <lashmanbala@gmail.com>
2026-08-11 14:57:17 +0530

    Idempotent fact table write to incase of rerun of spark job

--- pyspark/write.py
+++ pyspark/write.py
@@ -1,14 +1,27 @@
 from delta.tables import DeltaTable
 
-def write_delta_fact(df, tgt_dir):
-    path = f"{tgt_dir.rstrip('/')}/fact_events"
-    (
-    df.write.format("delta")
-    .partitionBy("year", "month", "day")
-    .mode("append")
-    .save(path)
-    )
 
+def write_delta_fact(spark, df, tgt_dir, key_column="event_id"):
+
+    path = f"{tgt_dir.rstrip('/')}/fact_events"
+
+    df = df.dropDuplicates([key_column])  # guard against dupes within the same batch
+
+    if not DeltaTable.isDeltaTable(spark, path):
+        df.write.format("delta").partitionBy("year", "month", "day").save(path)
+        return
+
+    delta_table = DeltaTable.forPath(spark, path)
+
+    (
+        delta_table.alias("t")
+        .merge(
+            df.alias("s"),
+            f"t.{key_column} = s.{key_column} AND t.year = s.year AND t.month = s.month AND t.day = s.day"
+        )
+        .whenNotMatchedInsertAll()
+        .execute()
+    )
 
 def merge_delta_dim(spark, df, tgt_dir, table_name, key_column):
 
